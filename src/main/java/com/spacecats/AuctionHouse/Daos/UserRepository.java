cbeff69ab0f566b5afaaf36735a9b141442d8a3f
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-12-09 17:06:27 -0500

    add user repository file

---
+++ src/main/java/com/spacecats/AuctionHouse/Daos/UserRepository.java
@@ -0,0 +1,14 @@
+package com.spacecats.AuctionHouse.Daos;
+
+import java.util.List;
+
+import com.spacecats.AuctionHouse.Models.Role;
+import com.spacecats.AuctionHouse.Models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // List<User> findUsersByRole(Role u_role);
+}
