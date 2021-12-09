5b370be584d20b1c9a70f09f7cb5123463e1f18f
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-12-09 18:17:25 -0500

    user service

--- src/main/java/com/spacecats/AuctionHouse/Services/UserService.java
+++ src/main/java/com/spacecats/AuctionHouse/Services/UserService.java
@@ -1,28 +1,42 @@
 package com.spacecats.AuctionHouse.Services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.spacecats.AuctionHouse.Daos.UserRepository;
 import com.spacecats.AuctionHouse.Exceptions.UNFException;
+import com.spacecats.AuctionHouse.Models.Role;
 import com.spacecats.AuctionHouse.Models.User;
-
 
 @Service
 public class UserService {
-	private List<User> users = new ArrayList<>();
+	
+	private UserRepository ur;
+	
+	@Autowired
+	public UserService(UserRepository ur) {
+		this.ur = ur;
+	}
 	
 	public List<User> getAllUsers(){
-		return users;
+		return ur.findAll();
 	}
 	
 	public User getUserById(int id) {
-		for(User u : users) {
-			if(u.getU_id() == id) {
-				return u;
-			}
-		}
-		throw new UNFException();
+		return ur.findById(id).orElseThrow(UNFException::new);
 	}
+	
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void createUser(User u) {
+		// bunch of logic
+		ur.save(u);
+	}
+	
+	// public List<User> getUserByRole(Role role){
+	// 	return ur.findUsersByRole(role);
+	// }
 }
